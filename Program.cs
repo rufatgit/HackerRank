2850307f3c11b4543b7f5d9839c09a06751d8c7c
rufatgit <93449554+rufatgit@users.noreply.github.com>
2023-03-04 22:20:36 +0100

    Add files via upload
    https://www.hackerrank.com/contests/projecteuler/challenges/euler001/problem?isFullScreen=true

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+
+
+Console.WriteLine("How many times you want to repeat process? ");
+
+int test_cases = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < test_cases; i++)
+{
+    Console.WriteLine("What is limit? ");
+    double limit =Convert.ToDouble(Console.ReadLine());
+
+    Stopwatch sw = new Stopwatch();
+    sw.Start();
+    Console.WriteLine("Gauss Sum: " + Function(limit, 3, 5, 15));
+    sw.Stop();
+    Console.WriteLine("Elapsed total milliseconds with Gauss Sum: "+sw.Elapsed.TotalMilliseconds.ToString("N0"));
+
+    Stopwatch sw2 = new Stopwatch();
+    sw2.Start();
+    Console.WriteLine("Brute Force way: " + Sum(limit));
+    sw2.Stop(); 
+    Console.WriteLine("Elapsed total milliseconds with Brute Force approach: "+sw2.Elapsed.TotalMilliseconds.ToString("N0"));
+
+
+}
+
+static double Gauss_Sum(double divisibility_number, double limit)
+{
+    double n = Math.Floor((limit - 1) / divisibility_number);
+    double k = (n / 2)  * (n + 1);
+
+    return divisibility_number * k;
+
+}
+
+
+static double Function(double limit, double three, double five, double fifteen)
+{
+    return Gauss_Sum(three, limit) + Gauss_Sum(five, limit) - Gauss_Sum(fifteen, limit);
+
+}
+
+
+
+static double Sum(double limit)
+{
+    double sum = 0;
+
+    for (int i = 0; i < limit; ++i)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+
+
+    }
+
+    return sum;
+
+}
+
+
